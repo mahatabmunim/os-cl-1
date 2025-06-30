7d9355be9fbbc070e422803cc6663bf129ba5b73
MAHATAB UDDIN <128234933+mahatabmunim@users.noreply.github.com>
2025-07-01 00:47:48 +0600

    Add files via upload

---
+++ class/class1.php
@@ -0,0 +1,43 @@
+<?php
+
+// //print fast code
+// echo "hello world";
+
+// //variable
+// $name = "mahatab";
+// echo PHP_EOL;
+// echo $name;
+
+// //variable change
+// $name = "mahatab";
+// $name = "munim";
+// echo $name;
+
+// //constant variable
+// const name = 'MUNIM';
+// echo name;
+
+// //define variable
+// define('PROJECT_NAME','web devlopment');
+// echo PROJECT_NAME;
+
+//  php single or double comment
+// echo "mahatab \n";f
+// echo "mahatab";
+
+//output printinfg
+// echo "this is print form echo \n";
+// print "this is print from print \n";
+// printf ("this is printf from printf \n");
+// sprintf ("this is sprintf from sprintf");
+
+//data type
+//string
+// $world = 'Bangladesh';
+// var_dump ('world');
+// echo "I Love $world";
+
+//  //integer
+// $price = 10000;
+// var_dump($price);
+
